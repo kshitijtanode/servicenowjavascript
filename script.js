bdff4df614cd3fe9694ed796d3486aaf104d5845
kshitijtanode <kshitijtanode@gmail.com>
2025-07-23 13:09:05 +0530

    variables

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log("Hello world...!");
+
+//alert("hello world....!");
+
+//confirm("would you like to join javascript");
+//notesfor JS//
+
+
---
+++ variables.js
@@ -0,0 +1,32 @@
+// Variables are containers for storing data//
+// X = 100;
+//console.log(x);
+// var let const
+
+// var studentname ="tonystark";
+// let schoolname ="XYz school";
+// const classmate = 10;
+
+// console.log(studentname);
+// console.log(schoolname);
+// console.log(classmate);
+
+//var
+// var num =100;
+// var num = 200;
+// consol.log(num);
+
+//let
+// let num=100;
+// num= 200; //automatically
+// console.log(num);
+
+//const
+// const num = 100;
+// num 100; // automatically
+// console.log(num);
+
+{
+    let num=100;
+    console.log(num);
+}
