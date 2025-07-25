9b9428ad79fa6b69a8b71422f5cbaeb09806f54b
kshitijtanode <kshitijtanode@gmail.com>
2025-07-25 20:25:39 +0530

    array

---
+++ array.js
@@ -0,0 +1,78 @@
+// An array holds  more  than one value
+// collection of items is known as array
+
+// let arr = ["one","two","three","four","five","six"];
+// console.log(arr);
+
+
+//length
+// let arr = ["one","two","three","four","five"];
+// let res = arr.length;
+// console.log(res);
+
+// let arr = ["one","two","three","four","five"];
+// // console.log(arr);
+// // console.log(arr[1]);
+
+// arr[2] ="update";
+// console.log(arr);
+
+
+//push
+// let fruits = ["mango","banana","kiwi","grape"];
+// console.log(fruits.push("chikoo")); //push method wriitens new length
+// console.log(fruits);
+
+
+
+//pop
+// let fruits = ["mango","banana","kiwi","grape"];
+// fruits.pop();
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+
+//toString
+// let fruits = ["mango","banana","kiwi","grape"];
+// console.log(fruits);
+// let res = fruits.toString();
+// console.log(res);
+
+
+//concat
+// let fruits = ["mango","banana","kiwi","grape"];
+// let fruits2 =["Apple","Orange","lemon","peru"];
+// console.log(fruits);
+// console.log(fruits2);
+
+// let  res = fruits.concat(fruits2);
+// console.log(res);
+
+
+//unshift
+// let fruits = ["mango","banana","kiwi","grape"];
+// fruits.unshift("Papaya");
+// console.log(fruits);
+
+
+// //shift
+// let fruits = ["mango","banana","kiwi","grape"];
+// fruits.shift();
+// console.log(fruits);
+
+
+// let arr = [1,2,3,4,5,6,[7,8,9]];
+// console.log(arr[6][2]);
+
+// let arr = [1,2,3,4,5,6,7,[8,9,10,11,[12,13,14,[15,16,17],18,19]]]
+
+// console.log(arr[7][4][4]);
+
+//string to arry
+let str ="1,2,3,4,5,6,";
+console.log(str.split());
