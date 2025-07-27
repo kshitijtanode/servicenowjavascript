7fb8e5b0226cba793410eba5b1382cf2eb1aee64
kshitijtanode <kshitijtanode@gmail.com>
2025-07-27 18:49:39 +0530

    object

---
+++ objects.js
@@ -0,0 +1,66 @@
+// objects
+
+// let studentinfo = {};
+// console.log(studentinfo);
+
+// let studentinfo = {
+//     key:value,
+//     key:value,
+//     key:value,
+//     key:value
+// }
+
+
+
+
+//  let studentinfo = {
+//     Fname : "Tony",
+//     Lname : "Stark",
+//     RollNo :  2001,
+//     isPass :  true,
+//     Grade : "B"
+//  }
+//  console.log(studentinfo);
+
+
+//  let studentinfo = {
+//     Fname : "Tony",
+//     Lname : "Stark",
+//     RollNo :  2001,
+//     isPass :  true,
+//     Grade : "B"
+//  }
+//  let res = studentinfo.isPass
+//  console.log(res);
+
+
+
+//  let studentinfo = {
+//     Fname : "Tony",
+//     Lname : "Stark",
+//     RollNo :  2001,
+//     isPass :  true,
+//     Grade : "B"
+// }
+//   studentinfo.Fname = "ram";
+//    studentinfo.RollNo =2002;
+//     studentinfo.Lname = "thar";
+//  console.log(studentinfo);
+
+
+
+
+// destructuring
+
+ let studentinfo = {
+    Fname : "Tony",
+    Lname : "Stark",
+    RollNo :  2001,
+    isPass :  true,
+    Grade : "B"
+ }
+ let{Fname, Lname, Grade} = studentinfo;
+ console.log(Fname);
+
+
+
