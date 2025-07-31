6659a433a24ef70de4d584f258e0201fd3464130
kshitijtanode <kshitijtanode@gmail.com>
2025-07-31 20:25:45 +0530

    function

---
+++ function.js
@@ -0,0 +1,108 @@
+// A JavaScript function is a block of code designed to perform a particular task.
+
+// A JavaScript function is executed when "something" invokes it (calls it).
+
+
+
+
+// function addSum(){
+//     console.log("hello this is normal function..");
+// }
+// addSum();  // invoke, call etc..
+
+
+
+
+// function addSum(){
+//     console.log(5 + 5);
+// }
+// addSum();
+
+
+
+// parameters and arguments
+// function addSum(num){
+//     console.log(num);
+// }
+// addSum(1000);
+
+
+// function addSum(num1, num2){   // parameters
+//     console.log(num1 +num2);
+// }
+// addSum(1000,2000);   //arguments
+
+//return keyword
+
+
+//  function addSum(num1, num2){  
+//     let res = num1 * num2;
+//     console.log("hello this is normal function..");
+//     return res;
+//     console.log("hello this is normal function..");
+
+// }
+// console.log(addSum(100,500));
+
+
+//rest operator
+
+
+// function exp(...para){
+//     return para;
+// }
+// console.log(exp(200,300,400,700,900,3300,800))
+
+
+// function addSum(){
+       //code
+
+//}
+
+
+//Arrow function
+
+// const addSum = (a,b) => {
+//     return a+b; 
+// }
+// console.log(addSum(12,13));
+
+
+// const addSum = () => console.log("one line arrow");
+// addSum()
+
+
+//IIFE
+
+// (function addSum(){
+//     console.log("This is IIFE");
+
+// })()
+
+
+
+// (function addSum(a,b){
+//     console.log(a + b);
+
+// })(12,17)
+
+
+
+(function addSum(){
+    console.log("This is IIFE");
+
+})();
+
+
+(function addSum(){
+    console.log("This is IIFE 2nd");
+
+})();
+
+
+
+
+
+
+
+
