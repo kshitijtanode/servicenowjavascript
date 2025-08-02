5351ad152a3f34b7530bfa95584fa885dae4ece3
kshitijtanode <kshitijtanode@gmail.com>
2025-08-02 12:41:13 +0530

    loop

---
+++ conditions.js
@@ -0,0 +1,65 @@
+//Conditional statements are used to perform different actions based on different conditions.
+//Use if to specify a block of code to be executed, if a specified condition is true
+// Use else to specify a block of code to be executed, if the same condition is false
+// Use else if to specify a new condition to test, if the first condition is false
+
+// if(condition){
+//       //execute  if condition is true.
+// }else{
+//     //execute if condition is false
+// }
+
+
+// let age = 20;
+// if(age>18){
+//     console.log("age is greater than 18");
+// }else{
+//     console.log("less than 18")
+// }
+
+
+// if(condition){
+
+// }else if(condition){
+
+// }else{
+
+// }
+
+
+//switch
+// let currDate = new Date().getHours();
+// console.log(currDate);
+
+// switch (new Date().getDay()) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//      day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+// }
+
+
+//ternary operator
+
+//condition / true : false
+
+// true ? console.log("condition is true") : console.log("condition is false");
+
+
+
---
+++ loops.js
@@ -0,0 +1,80 @@
+// loops can execute a block of code a number of times.
+
+
+//for loop
+
+// for(let i = 0; i<=  20; i++){
+//     console.log(i);
+// }
+
+
+//while
+// let i =1;
+
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+
+//do while loop
+// let i=1;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= 10);
+
+
+
+//for of loop
+
+// let fruits = ["apple","banana","chikoo","grapes","watermelon","kiwi"];
+// for(const element of fruits) {        //elemts kahipn gheu shkto and frits  he object ahe
+//     console.log(element);
+//     }
+
+
+//for in loop
+
+// const studentInfo = {
+//     Fname : "sansa",
+//     Lname : "stark",
+//     rollNo : 1010,
+//     classname : "10A",
+//     marksobt : "450/700",
+//     grade : "A++"
+
+// }
+// for (let key in studentInfo){
+//     console.log(studentInfo[key])
+// }
+
+
+
+
+// const studentInfo = {
+//     Fname : "sansa",
+//     Lname : "stark",
+//     rollNo : 1010,
+//     classname : "10A",
+//     marksobt : "450/700",
+//     grade : "A++"
+
+// }
+// for (let key in studentInfo){
+//      let res = `the key in the student object is ${key} and value is ${studentInfo[key]}`
+
+//     console.log(res);
+// }
+
+
+//for each loop
+
+// let fruits = ["apple","banana","chikoo","grapes","watermelon","kiwi"];
+
+// fruits.forEach((i) => {
+//     console.log(i);
+// })
+
+
